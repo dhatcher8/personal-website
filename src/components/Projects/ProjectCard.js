32b02da3ab03bd947e9341194b6677689bfb9e7a
Drew Hatcher <drewhatcher@gmail.com>
2020-07-28 11:42:05 -0400

    Finish project card element

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-import githubIcon from '../../assets/images/SVG/github-icon.svg';
+import githubWhite from '../../assets/images/SVG/github-white.svg';
+import webLinkIcon from '../../assets/images/SVG/weblink-icon.svg';
 
 import './ProjectCard.scss';
 
@@ -11,8 +12,8 @@ class ProjectCard extends Component {
 
     render() {
 
-        const bullet_points = this.props.project_details[0].bullets.map((bullet) =>
-            <li className="project-bullet-point"> &gt; {bullet}</li>
+        const project_tags = this.props.project_details[0].tags.map((tag) =>
+            <li className="project-tags">{tag}</li>
             );
 
         return (
@@ -25,18 +26,21 @@ class ProjectCard extends Component {
                     <div className="project-text-overlay">
                         <div className="project-top-text">
                             <div className="project-title-text">{this.props.project_details[0].title}</div>
-                            <div className="project-role-text">{this.props.project_details[0].role}</div>
-                            <div className="project-location-text">{this.props.project_details[0].location}</div>
+                            {/* <div className="project-role-text">{this.props.project_details[0].role}</div>
+                            <div className="project-location-text">{this.props.project_details[0].location}</div> */}
                             <div className="project-date-text">{this.props.project_details[0].date}</div>
                         </div>
                         <div className="project-body-text">
-                            <div className="project-bullet-point">
+                            <div className="project-description">
                                 {this.props.project_details[0].description}
                             </div>
+                            {project_tags}
                             <div className="bottom-project-links">
-                                <img className="github-icon" src={githubIcon} alt="github" href="this.props.project_details[0].githubLink"></img>
-                                <div class="project-links-text">{this.props.project_details[0].githubLink}</div>
-                                <div class="project-links-text">{this.props.project_details[0].websiteLink}</div>
+                                <a href={this.props.project_details[0].githubLink} target="_blank" rel="noopener noreferrer"><img className="project-links-text" src={githubWhite} alt="github"></img></a>
+                                <a href={this.props.project_details[0].websiteLink} target="_blank" rel="noopener noreferrer"><img className="project-links-text" src={webLinkIcon} alt="website"></img></a>
+                                
+                                {/* <div class="project-links-text">{this.props.project_details[0].githubLink}</div>
+                                <div class="project-links-text">{this.props.project_details[0].websiteLink}</div> */}
                             </div>
                         </div>
                     </div>
--- src/constants/projects-info/you-choose-muse-info.js
+++ src/constants/projects-info/you-choose-muse-info.js
@@ -6,13 +6,13 @@ export default [
     "image":YouChooseMuse,
     "title":"You Choose, Muse",
     "date":"Summer 2020",
-    "description":"This is the description for You Choose, Muse",
+    "description":"You Choose, Muse is a web app that crafts recommendation playlists based on a user’s parameters. The user can login to Spotify to save thier playlist as well as see their top tracks, top artists, and recently played tracks.",
     "githubLink":"https://github.com/dhatcher8/you-choose-muse",
     "websiteLink":"https://you-choose-muse.herokuapp.com/",
-    "bullets":[
-        "Hold office hours, teach weekly recitation, and grade course assignments",
-        "Implementing active learning techniques and encouraging collaborative learning",
-        "Utilizing my ability to communicate and empathize with others to give back to the community and to improve my own leadership skills"
+    "tags":[
+        "React",
+        "Node.js",
+        "Spotify Web API",
         ]
     },
 ];
