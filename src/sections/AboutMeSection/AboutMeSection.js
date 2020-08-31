daee5f2f5486fd862fb6e3b92fbd8770270329d1
Drew Hatcher <drewhatcher@gmail.com>
2020-08-31 19:07:15 -0400

    add personal image to about me

--- src/sections/AboutMeSection/AboutMeSection.js
+++ src/sections/AboutMeSection/AboutMeSection.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import aboutMeImage from '../../assets/images/about/personal-image.jpg';
+
 import './AboutMeSection.scss';
 
 const about_me_section = props => {
@@ -9,15 +11,25 @@ const about_me_section = props => {
                 <h2 className="section-header">
                     about me.
                 </h2>
-                <p className="about-me-body-text">
-                    Hi, nice to meet you! My name is Drew Hatcher. I’m currently a 4th year Computer Science student at Georgia Tech and will be starting a Master’s in Computer Science in the Spring of 2021. My passion in school outside of learning is teaching. For the past three semester’s I’ve been a teaching assistant for Discrete Mathematics and I plan to TA database design for the next three semesters. 
-                </p>
-                <p className="about-me-body-text">
-                    I’m persuing a career in software development with an interest in back-end work. Outside of my career I have a passion for art, namely photography and graphic design.
-                </p>
-                <p className="about-me-body-text">
-                    Keep scrolling to learn more about me!
-                </p>
+                <div className="about-me-text-and-image">
+                    <div className="about-me-description">
+                        <p className="about-me-body-text">
+                            Hi, nice to meet you! My name is Drew Hatcher. I’m currently a 4th year Computer Science student at Georgia Tech and will be starting a Master’s in Computer Science in the Spring of 2021. My passion in school outside of learning is teaching. For the past three semester’s I’ve been a teaching assistant for Discrete Mathematics and I plan to TA database design for the next three semesters. 
+                        </p>
+                        <p className="about-me-body-text">
+                            I’m persuing a career in software development with an interest in back-end work. Outside of my career I have a passion for art, namely photography and graphic design.
+                        </p>
+                        <p className="about-me-body-text">
+                            Keep scrolling to learn more about me!
+                        </p>
+                    </div>
+                    <div className="about-me-image-wide">
+                        <img className="about-me-jpg-large" src={aboutMeImage} alt=""/>
+                    </div>
+                </div>
+                <div className="about-me-image-narrow">
+                    <img className="about-me-jpg-small" src={aboutMeImage} alt=""/>
+                </div>
             </div>
         </div>
     );
