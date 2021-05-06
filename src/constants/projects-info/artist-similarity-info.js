562f0239d1cbd9475f787392aa9851601dfb9f82
Drew Hatcher <drewhatcher@gmail.com>
2021-05-06 11:05:39 -0400

    Fix websiteLink for artist similarity

--- src/constants/projects-info/artist-similarity-info.js
+++ src/constants/projects-info/artist-similarity-info.js
@@ -8,7 +8,7 @@ export default [
     "date":"March - April 2021",
     "description":"A user focused data science project that utilizes two Kaggle datasets along with algorithms such as KNN to find an artist's most similar artists based on a user selected metric.",
     "githubLink":"none",
-    "websiteLink":"hhttps://colab.research.google.com/drive/15iMDki5B58ub0LvVLd_CS9UNT7sDqUb8?usp=sharing",
+    "websiteLink":"https://colab.research.google.com/drive/15iMDki5B58ub0LvVLd_CS9UNT7sDqUb8?usp=sharing",
     "backgroundColor":"#202020",
     "tags":[
         "Spotify Web API",
